4dff70885f2c20a6ff86cf234e7e1d3880b571f7
Prachiti Barge <prachitibarge01@gmail.com>
2020-10-11 14:19:46 +0530

    added few functions to length.py

--- length.py
+++ length.py
@@ -3,6 +3,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--inch-to-cm', type=float, dest="inch_to_cm")
 parser.add_argument('--cm-to-inch', type=float, dest="cm_to_inch")
+# args parser for cm to mm converter
+parser.add_argument('--cm-to-mm', type=float, dest="cm_to_mm")
 args = parser.parse_args()
 
 
@@ -23,6 +25,12 @@ def cm_to_inch_helper():
     if cm:
         cm_to_inch(cm)
 
+# helper function for checking args for cm_to_mm function
+def cm_to_mm_helper():
+    cm_mm = args.cm_to_mm
+    if (cm_mm):
+        cm_to_mm(cm_mm)
+
 
 # Converter functions
 def inch_to_cm(inch):
@@ -31,6 +39,10 @@ def inch_to_cm(inch):
 
 def cm_to_inch(cm):
     print(f'{cm} cm in inch is: {cm/2.54} inch')
+
+# function to convert cm to mm
+def cm_to_mm(cm_mm):
+    print(f'{cm_mm}cm in mm is: {cm*10} mm')
 
 
 if __name__ == "__main__":
